6375196caeab8d8a63eb629189e95999b430d89e
Hadi Hassan <hadihassan09@gmail.com>
2019-08-28 19:59:58 +0300

    First Task Completed

--- EmplyeReport.js
+++ EmplyeReport.js
@@ -2,16 +2,35 @@ class WorkShop {
     constructor(){
         this.employes = [];
     }
-    addEmploye(employeInfo){
-        this.employes.push(employeInfo);
+    addEmploye(FName, ageInNum){
+        this.employes.push({name: FName, age: ageInNum});
     }
-
+    workOnSunday(){
+        let len = this.employes.length;
+        let canWork = [];
+        for(let i=0;i < len; i++){
+            if(this.employes[i].age > 18)
+                canWork.push(this.employes[i]);
+        }
+        return canWork;
+    }
 }
 
 
 QUnit.test("Adding Employes",(assert) => {
     const work = new WorkShop();
-    work.addEmploye({name: 'Max', age: 17 });
+    work.addEmploye('Max',17);
     assert.ok(work.employes[0].name === 'Max');
     assert.ok(work.employes[0].age === 17);
+});
+
+QUnit.test("Checking Function for workers +18",(assert) => {
+    const work = new WorkShop();
+    work.addEmploye('Max', 17);
+    work.addEmploye('Sepp',18);
+    work.addEmploye('Nina', 15);
+    work.addEmploye('Mike', 51);
+
+    assert.ok(work.workOnSunday()[0].name === 'Mike');
+    assert.ok(work.workOnSunday()[0].age === 51);
 });
