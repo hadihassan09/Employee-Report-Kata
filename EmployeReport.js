e5030670271b8195be84f6b5e85c4b2f81bdd01b
Hadi Hassan <hadihassan09@gmail.com>
2019-08-28 21:10:15 +0300

    Refactoring Task Two

--- EmployeReport.js
+++ EmployeReport.js
@@ -8,6 +8,16 @@ var compare = (a, b) => {
     return 0;
 }
 
+var getOverEighteen = (array) => {
+    let canWork = [];
+    for(let i=0;i < array.length; i++){
+        if(array[i].age > 18)
+            canWork.push(array[i]);
+    }
+    return canWork;
+}
+
+
 class WorkShop {
     constructor(){
         this.employes = [];
@@ -18,18 +28,14 @@ class WorkShop {
     }
 
     workOnSunday(){
-        let len = this.employes.length;
-        let canWork = [];
-        for(let i=0;i < len; i++){
-            if(this.employes[i].age > 18)
-                canWork.push(this.employes[i]);
-        }
-        return canWork;
+        return getOverEighteen(this.employes);
     }
+
     sortByName(){
         this.employes.sort(compare);
         return 1;
     }
+
 }
 
 
@@ -66,4 +72,14 @@ QUnit.test("Sortng Array By Names, so its easier to find employes",(assert) => {
     assert.ok(work.employes[1].name === 'Mike');
     assert.ok(work.employes[2].name === 'Nina');
     assert.ok(work.employes[3].name === 'Sepp');
+})
+
+QUnit.test("Making all names capitalized so I can read it better",(assert) => {
+    const work = new WorkShop();
+    work.addEmploye('Sepp',18);
+    work.addEmploye('Max', 17);
+    work.addEmploye('Nina', 15);
+    work.addEmploye('Mike', 51);
+
+
 })
